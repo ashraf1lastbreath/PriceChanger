81a4201965ecfb5935d77afaf86a3bb28f7596a3
Nickyg <nicky.gurbani@gmail.com>
2017-05-11 18:33:06 +0530

    encoding change html

--- scrapper.py
+++ scrapper.py
@@ -137,7 +137,7 @@ def snapdeal_scrapper(url):
     try :
          #retrieve Item 
          item = soup.find('h1', attrs={'class': 'pdp-e-i-head'})   #to find out only the tag we are interested in
-         print item
+         #print item
          item_txt = item.get_text( ).encode(sys.stdout.encoding, errors='replace' )  #to retrieve the item name text
          item_txt = item_txt.strip( )          # to remove trailing and leading whitespaces
          #print item_txt 
